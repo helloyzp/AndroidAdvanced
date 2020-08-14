78bfd1e20b3ac985fef869b65f2d60f10164d75c
yzp <1048023269@qq.com>
2020-08-14 17:55:54 +0800

    数组也是Object对象

--- app/src/main/java/com/example/javaadvanced/jvm/ex1/MemoryArea.java
+++ app/src/main/java/com/example/javaadvanced/jvm/ex1/MemoryArea.java
@@ -11,6 +11,15 @@ public class MemoryArea {
     public static final Object obj2 = new Object();
     //str是静态变量，存储在方法区，"abc"是字面量，存储在方法区的运行时常量池中。
     public static String str = "abc";
+    //array是静态变量，存储在方法区，new int[10]创建了一个数组对象，存储在堆中
+    public static int[] array = new int[10];
+
+    public static void main(String[] args) {
+        int[] arr = new int[10];//arr引用变量存储在main方法的栈帧的局部变量表中，new int[10]创建了一个数组对象，存储在堆中
+        if(arr instanceof Object) {
+            System.out.println("数组是对象");
+        }
+    }
 
 
 }
