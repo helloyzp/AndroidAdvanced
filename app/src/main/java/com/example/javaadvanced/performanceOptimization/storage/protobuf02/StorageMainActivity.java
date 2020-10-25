5152afb7a9fa51e4dacb435c6f8b759ff24b5ff3
yzp <1048023269@qq.com>
2020-10-25 17:29:05 +0800

    存储优化protobuf

---
+++ app/src/main/java/com/example/javaadvanced/performanceOptimization/storage/protobuf02/StorageMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.javaadvanced.performanceOptimization.storage.protobuf02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.javaadvanced.R;
+
+public class StorageMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_storage);
+    }
+}
