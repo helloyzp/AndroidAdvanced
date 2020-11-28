d926de94519678a2250e4ed0f17c8fd8fd22ff7e
yzp <1048023269@qq.com>
2020-11-28 18:12:55 +0800

    dns

---
+++ app/src/main/java/com/example/javaadvanced/network/okhttp/dns/HttpDns.java
@@ -0,0 +1,28 @@
+package com.example.javaadvanced.network.okhttp.dns;
+
+import android.util.Log;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
+
+import okhttp3.Dns;
+
+public class HttpDns implements Dns {
+    private String TAG = "HttpDns";
+
+    private static final Dns SYSTEM = Dns.SYSTEM;
+
+    @Override
+    public List<InetAddress> lookup(String hostname) throws UnknownHostException {
+        Log.e(TAG, "lookup(), hostname=" + hostname);
+/*        String ip = DNSHelper.getIpByHost(hostname);
+        if (ip != null && !ip.equals("")) {
+            List<InetAddress> inetAddresses = Arrays.asList(InetAddress.getAllByName(ip));
+            Log.e("HttpDns", "inetAddresses:" + inetAddresses);
+            return inetAddresses;
+        }*/
+        return SYSTEM.lookup(hostname);
+    }
+}
