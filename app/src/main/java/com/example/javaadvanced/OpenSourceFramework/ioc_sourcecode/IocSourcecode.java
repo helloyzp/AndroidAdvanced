8cd325ee3cc1ee44affe79a25066de3cbad63be9
yzp <1048023269@qq.com>
2020-12-18 18:21:54 +0800

    ioc

--- app/src/main/java/com/example/javaadvanced/OpenSourceFramework/ioc_sourcecode/IocSourcecode.java
+++ app/src/main/java/com/example/javaadvanced/OpenSourceFramework/ioc_sourcecode/IocSourcecode.java
@@ -2,6 +2,10 @@ package com.example.javaadvanced.OpenSourceFramework.ioc_sourcecode;
 
 /**
  * 源码时注入，即各种Android Studio插件就是源码时注入
+ * 参见：
+ * https://github.com/helloyzp/GenerateJavabeanByString
+ * https://github.com/helloyzp/ButterknifePluginDemo *
+ * 这两个插件的源码
  */
 public class IocSourcecode {
 }
